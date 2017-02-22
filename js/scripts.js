24aa5dc8594ee715c4f0064c57010c92eddae69d
wojchmielew <woj.chmielew@gmail.com>
2017-02-22 18:09:29 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-// plik scripts.js
-var name = prompt('Wpisz twoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var triangle1Area = getTriangleArea(4, 11);
+console.log("Powierzchnia 1 trójkąta: ", (triangle1Area) )
+var triangle2Area = getTriangleArea(2, -8);
+console.log("Powierzchnia 2 trójkąta: ", (triangle2Area) )
+var triangle3Area = getTriangleArea(7, 7);
+console.log("Powierzchnia 3 trójkąta: ", (triangle3Area) )
+
+function getTriangleArea(a, h) {
+        
+    if (a < 0 || h < 0) {
+        console.log('Nieprawidłowe dane obiektu poniżej'); 
+    }
+    return (a * h / 2)
+        
+}
