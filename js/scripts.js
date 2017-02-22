83c4f746e067d5278c69ae082dab3427b6227bdd
wojchmielew <woj.chmielew@gmail.com>
2017-02-22 18:12:25 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-// plik scripts.js
-var name = prompt('Wpisz twoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var textBasic = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var textAnimal = 'Zielone słonie';
+var textAnimalUpperCased = textAnimal.toUpperCase();
+console.log(textAnimalUpperCased);
+
+var textChange = textBasic.replace('Papugi', textAnimalUpperCased);
+var halfText = textBasic.length / 2
+
+console.log(textChange);
+console.log(textChange.slice(0, textChange.length / 2));
+
+var textZajace = 'Fioletowe zające';
+var textKucharki = 'Lewitujące kucharki'
+var changeFirstWord = textBasic.split(' ', 1);
+var changeWordFirst = textBasic.split(' ', 1);
+var textChangeBasic = textBasic.replace(changeFirstWord, textZajace);
+var textChangeKucharki = textBasic.replace(changeWordFirst, textKucharki);
+
+console.log(textChangeBasic);
+console.log(textChangeKucharki);
+console.log(textBasic);
