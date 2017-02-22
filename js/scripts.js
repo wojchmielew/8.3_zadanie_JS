69db621a070f09a3ea637ec76d17f21c064e3086
Wojciech Chmielewski <wojtek@Wojciechs-iMac.home>
2017-02-22 16:39:52 +0100

    com2

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+var name = prompt('Wpisz swoje imię'); // zadeklarowanie zmiennej o nazwie name
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+/*
+  console.log(‘wartość zostanie wyświetlona w zakładce’);
+*/
