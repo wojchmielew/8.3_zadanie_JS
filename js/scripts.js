7443128a1ea3e39d7ac5c32c858630a296b0d12d
wojchmielew <woj.chmielew@gmail.com>
2017-02-22 18:15:46 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-// plik scripts.js
-var name = prompt('Wpisz twoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+function rysujChoinkę(h) {
+    for (n = 0; n <= h; n++) {
+        var star = '';
+        for(j = 1; j <= (2 * n - 1); j++) {
+            star = star + '*';
+        }
+    console.log(star);
+    }
+}
